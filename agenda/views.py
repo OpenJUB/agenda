eca00f319200887f715013b9a9f139ecb26e4938
Mihail Tarigradschi <m.tarigradschi@gmail.com>
2018-11-17 18:05:32 +0100

    Added minimal backend

---
+++ agenda/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+
+from .models import AgendaItem
+
+
+def index(request):
+    if request.method == 'POST' and request.POST.get('addItem'):
+        itemText = request.POST.get('addItem', '')
+        if itemText != '':
+            newItem = AgendaItem.objects.create(text=itemText)
+            newItem.save()
+    aItems = list(AgendaItem.objects.all())
+    return render(request, 'index.html', {'agendaItems': aItems})
